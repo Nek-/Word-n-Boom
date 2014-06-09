829280d9a79cab38b22ca7852434a4016d64a903
Nek (Maxime Veber) <nek.dev@gmail.com>
2014-06-09 03:42:39 +0200

    Server should now work (in theory)

--- js/server.js
+++ js/server.js
@@ -1,6 +1,7 @@
 var app = require('http').createServer(handler);
 var io  = require('socket.io')(app);
 var fs  = require('fs');
+var lineReader = require('line-reader');
 
 app.listen(8888);
 
@@ -18,7 +19,8 @@ Array.prototype.removeValue = function(val) {
 
 var messages = [],
     players  = []
-    game     = null;
+    game     = null,
+    dictionary;
 
 io.on('connection', function (socket) {
     var user = null;
@@ -80,7 +82,7 @@ function Game () {
         setTimeout(this.realStart.bind(this), 30000);
     };
 
-    this.addPlayer = function(pseudo) {
+    this.addPlayer = function (pseudo) {
         this.players.push(pseudo);
     };
 
@@ -97,7 +99,7 @@ function Game () {
         }
     };
 
-    this.startRound = function (player) {
+    this.startRound = function () {
         io.emit('game.round', { player: this.currentPlayer, letters: this.generateLetters() });
 
         // 30min chaque tour dure 30s
@@ -107,30 +109,74 @@ function Game () {
     this.answer = function(data, socket) {
         if (isValidAnswer(data.answer)) {
             this.nextPlayer();
-            io.emit('game.turn', {player: this.currentPlayer, letters: this.generateLetters() })
+            io.emit('game.turn', { player: this.currentPlayer, letters: this.generateLetters() })
         } else {
             socket.emit('game.tryAgain', {});
         }
     };
 
     this.endRound = function () {
-        io.emit('game.endRound', {pseudo: this.currentPlayer});
+        io.emit('game.endRound', { pseudo: this.currentPlayer });
         this.players.removeValue(this.currentPlayer);
+
+        if (this.player.length < 2) {
+            io.emit('game.winner', {pseudo: this.player[0]});
+        } else {
+            this.currentPlayer = this.player[0];
+            this.startRound();
+        }
     };
 
     this.generateLetters = function () {
+        var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+            letterLength = letters.length,
+            imax = Math.floor(Math.random()*2) + 1,
+            res  = '';
 
+        for (var i = 0; i <= imax; i++) {
+            res += letters.charAt(Math.floor(Math.random() * letterLength));
+        }
+
+        return res;
     };
 
     this.nextPlayer = function () {
+        var index = this.players.indexOf(this.currentPlayer);
 
+        if (++index == this.players.length) {
+            this.currentPlayer = this.players[0];
+        } else {
+            this.currentPlayer = this.players[index];
+        }
     };
 
-    function isValidAnswer() {
+    function isValidAnswer (answer) {
+        if (dictionary.indexOf(answer) !== -1) {
+            return true;
+        }
 
+        return false;
     }
 
+    /**
+     * Nettoie la mémoire
+     */
     this.end = function() {
         game = null;
     };
+
 }
+
+///////////////////////////////////////////
+//                                       //
+// Préchargement du dictionnaire         //
+// en mémoire pour un accès plus rapide  //
+//                                       //
+///////////////////////////////////////////
+
+lineReader.eachLine('dictionary', function (line, last) {
+    var word = line.trim();
+    if (word !== '') {
+        dictionary.push(word);
+    }
+});
